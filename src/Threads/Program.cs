67180a249f07e263e7fface1cdffc3d406db3108
techwick73 <techwick73@gmail.com>
2019-10-15 15:13:09 +0100

    First commit

---
+++ src/Threads/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Function1();
+            Function2();
+        }
+
+        static void Function1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine($"Function 1 executed: {i.ToString()}"); ;
+            }
+
+        }
+        static void Function2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine($"Function 2 executed: {i.ToString()}"); ;
+            }
+
+        }
+    }
+}
